e1c6bbbd5ed9111fbf00016f6ea8ee0cc248f09a
Andrew Morgan <andrew.morgan@mongodb.com>
2017-02-23 10:11:12 +0000

    safe config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-	appId: "TBD",
-	mongopopAPI: "http://localhost:3000/pop/"
+	appId: "amzn1.ask.skill.0a271327-baf4-426c-a9c6-ee7d630c0deb",
+	mongopopAPI: "http://clusterdb.myddns.me:3000/pop/"
 };
 
 module.exports = config;
